0679dc47e17f497fa90c66a0a4ccc03e35568293
rmigranov <rust-skate@mail.ru>
2017-12-17 19:27:36 +0300

    Forms created

---
+++ questions/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Question
+"""
+class ProfileForm(forms.Form):
+    avatar = forms.ImageField(
+        widget = forms.FileInput(
+            attrs={'class': 'form-control-file'}
+        )
+    )
+"""
+"""
+    class QuestionForm(forms.ModelForm):
+    class Meta:
+"""
